b1f6133ff43f0e5d568213e159ea15dbca43a39f
nvembar <navin.vembar@gmail.com>
2016-11-25 11:11:27 -0500

    Added a bit more logging

--- broker/broker.go
+++ broker/broker.go
@@ -71,11 +71,20 @@ func (b *EC2Broker) Provision(context context.Context, instanceID string, detail
 		logger.Info("failed-provision-parse-parameters", lager.Data{"error": err.Error()})
 		return brokerapi.ProvisionedServiceSpec{}, brokerapi.ErrRawParamsInvalid
 	}
-	_, err = b.Manager.ProvisionAWSInstance(details.PlanID, parameters.AMIID, parameters.SecurityGroupID, parameters.SubnetID, parameters.AssignPublicIP, instanceID)
+	logger.Info("attempting-provision", lager.Data{
+		"plan_id":             details.PlanID,
+		"service_instance_id": instanceID,
+		"ami_id":              parameters.AMIID,
+		"security_group_id":   parameters.SecurityGroupID,
+		"subnet_id":           parameters.SubnetID,
+		"assign_public_ip":    parameters.AssignPublicIP,
+	})
+	awsID, err := b.Manager.ProvisionAWSInstance(details.PlanID, parameters.AMIID, parameters.SecurityGroupID, parameters.SubnetID, parameters.AssignPublicIP, instanceID)
 	if err != nil {
 		logger.Info("failed-provision-creation", lager.Data{"error": err.Error()})
 		return brokerapi.ProvisionedServiceSpec{}, err
 	}
+	logger.Info("created-instance", lager.Data{"aws_instance_id": awsID, "instance_id": instanceID})
 
 	return brokerapi.ProvisionedServiceSpec{
 		IsAsync:       true,
